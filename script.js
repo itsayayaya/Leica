f5503526f58d26dc2253bf8d08faf14e3f7ba040
itsayayaya <itsayayaya@gmail.com>
2026-06-05 23:46:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const API_URL =
-"https://script.google.com/macros/s/AKfycbxpFT3dzd6qFDjsZ0tvQ-FNA2f4PoQ_p6ruS1zCaKqKB0mHpgwcnazaX7I5QRCgD_9Q6A/exec";
+"https://script.google.com/macros/s/AKfycby5AV0l_gZW1LGllAgLdb0FIKjudc4l6Rw0xxpv07VEOo-0v5UsfU85wPs5_CKOqxE0CQ/exec";
 
 async function loadGallery(){
 
