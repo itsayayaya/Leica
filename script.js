01e84fe5df8026085796c506359b32cc89c0ba18
itsayayaya <itsayayaya@gmail.com>
2026-06-06 12:31:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -61,6 +61,25 @@ document.getElementById("close")
       .style.display = "none";
 };
 
+<a
+  class="download"
+  href="${photo.download}"
+  target="_blank"
+  title="Download">
+
+<svg xmlns="http://www.w3.org/2000/svg"
+     width="20"
+     height="20"
+     fill="currentColor"
+     viewBox="0 0 16 16">
+
+  <path d="M.5 9.9v4.6a1 1 0 0 0 1 1h13a1 1 0 0 0 1-1V9.9h-1v4.6H1.5V9.9h-1z"/>
+
+  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3-.708-.708L8.5 10.293V1h-1v9.293L5.354 8.146l-.708.708 3 3z"/>
+
+</svg>
+
+</a>
 loadGallery();
 
 setInterval(loadGallery, 30000);
